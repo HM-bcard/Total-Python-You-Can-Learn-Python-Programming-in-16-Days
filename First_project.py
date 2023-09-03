2b1507dbd93cb5726a2defcd804d335299ce449a
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-03 23:02:28 +0200

    Add files via upload

--- First_project.py
+++ First_project.py
@@ -1,4 +1,10 @@
-print('Hello Wofrld!')
+print("This is one \"Fellini\" moment ")
 
+print("multi \n line")
 
+print("Print this \\ backslash")
 
+print('tell me your name: ')
+
+input("Tell me your name: ")
+input('Tell me your last name: ')
