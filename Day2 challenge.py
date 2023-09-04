0de863ffbf1b4b8461c0452e5c79d44cae860b16
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-05 01:23:57 +0200

    Add files via upload

---
+++ Day2 challenge.py
@@ -0,0 +1,4 @@
+employee_name = input('What\'s your name?')
+employee_sales = int(input('What were your today\'s sales? '))
+
+print(employee_name + '\'s ' + ' today\'s commission is:', employee_sales*13/100,  'USD')
