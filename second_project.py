2693b40584cd15e3172b2190a312398e99ddca2a
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-04 23:00:01 +0200

    Add files via upload

---
+++ second_project.py
@@ -0,0 +1,10 @@
+name = 'Tony Soprano'
+age = 51
+
+first_name = 'Julia'
+last_name = 'Roberts'
+full_name = first_name + ' ' + last_name
+
+course = 'Python'
+print('You are taking a ' + course + ' course')
+
