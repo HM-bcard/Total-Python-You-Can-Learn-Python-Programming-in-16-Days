45ea5682d2a76de8da5ec594b2d5ef3a0fc112ef
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-07 23:39:19 +0200

    Add files via upload

---
+++ str_format.py
@@ -0,0 +1,39 @@
+x = 10
+y = 5
+print("My numbers are" + str(x) + " and " + str(y))
+
+
+print("My numbers are {} and {}".format(x,y))
+
+print("My numbers are {} and {} and their sum is {}".format(x,y,x+y))
+
+
+
+color = "red"
+lic_plate = 541126
+# literal strings
+print(f"The car is {color} and it\'s license plate is {lic_plate}")
+
+
+# exercise one:
+
+associate_name = "Jesse Pinkman"
+associate_number = 399058
+
+print(f"Dear {associate_name}, your associate number is: {associate_number}")
+
+# exercise two:
+
+new_points = 350
+total_points = 1225
+
+print(f"You have earned {new_points} points! In total, you have accumulated {total_points} points")
+
+# exercise three
+
+previous_points = 875
+new_points = 350
+total_points = previous_points + new_points
+
+print(f"You have earned {new_points} points! In total, you have accumulated {total_points} points")
+
