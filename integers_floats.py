4c25c4ab30f238fc3525d266db3b7bf9cbbc7bda
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-07 23:11:11 +0200

    Add files via upload

---
+++ integers_floats.py
@@ -0,0 +1,16 @@
+my_number = 1 + 2
+print(my_number)
+
+print(type(my_number))
+
+my_number = 1 + 2.
+print(my_number)
+
+print(type(my_number))
+
+
+age = input("Tell me your age: ")
+print("Your age is" + age)
+new_age = 1 + 2
+print(age + 'plus two')
+
