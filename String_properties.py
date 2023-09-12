5a1fb71000fcf18c4f3300c1e238bad1210bafe1
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-12 19:24:36 +0200

    Add files via upload

---
+++ String_properties.py
@@ -0,0 +1,21 @@
+# immutable
+# can be concatenated (added )
+# checkeable content
+
+my_text = "hello world"
+print("hello" in my_text)
+
+name = "Harry"
+name = "Garry"
+print(name)
+
+name = "Harry"
+name1 = "Garry"
+print(name+name1)
+
+poem = """Thousand little white fish 
+as if boiled
+the color of water
+"""
+
+print("water" not in poem)
