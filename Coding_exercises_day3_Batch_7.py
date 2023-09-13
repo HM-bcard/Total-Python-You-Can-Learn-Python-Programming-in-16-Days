60f0ed97a56bc9c770d3d4a562b8680aad81ffd9
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-13 23:52:45 +0200

    Add files via upload

---
+++ Coding_exercises_day3_Batch_7.py
@@ -0,0 +1,17 @@
+# 1
+
+my_set_1 = {1, 2, "three", "four"}
+
+my_set_2 = {"three", 4, 5}
+
+my_set_3 = my_set_1.union(my_set_2)
+
+# 2
+
+raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+raffle.pop()
+
+# 3
+
+raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+raffle.add('Gunther')
