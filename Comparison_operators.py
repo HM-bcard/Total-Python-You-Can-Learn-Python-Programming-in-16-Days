bd2386aaa50fa9ee8dad8df3b3121209f0411078
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-15 06:11:03 +0200

    Add files via upload

---
+++ Comparison_operators.py
@@ -0,0 +1,9 @@
+my_bool = 'white' == 'white'
+print(my_bool)
+
+my_bool = 'white' == 'White'
+print(my_bool)
+
+my_bool = '100' == 100
+print(my_bool)
+
---
+++ Day3_coding_challenges_batch1.py
@@ -0,0 +1,19 @@
+# Challenge 1
+
+num1 = 36
+num2 = 17
+my_bool = bool(num1 >= num2)
+
+# Challenge 2
+
+num1 = 25**0.5
+num2 = 5
+my_bool = num1 == num2
+
+
+# Challenge 3
+
+num1 = 64*3
+num2 = 24*8
+my_bool = num1 != num2
+
