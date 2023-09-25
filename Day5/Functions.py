6428bb09dcd1705e60bd2387ee2db4d0a1132814
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-25 21:34:50 +0200

    Add files via upload

---
+++ Day5/Functions.py
@@ -0,0 +1,8 @@
+def my_function(name):
+    """
+    this function does xyz
+    :return:
+    """
+    print("Henlo" + name)
+
+my_function(" Boob")
--- Day5/Methods,help,documentation.py
+++ Day5/Methods,help,documentation.py
@@ -2,3 +2,7 @@ dic = {'key1': 100, 'key2': 500}
 
 dic.popitem()
 
+a = dic.popitem()
+
+print(a)
+print(dic)
