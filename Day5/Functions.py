7e2ff4aa37afb342845b462b081c1313aa32286e
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-26 23:09:10 +0200

    Add files via upload

--- Day5/Functions.py
+++ Day5/Functions.py
@@ -5,4 +5,33 @@ def my_function(name):
     """
     print("Henlo" + name)
 
+
 my_function(" Boob")
+
+
+def greet_person():
+    """
+    This function is used to greet people
+    another line
+    it's a good habit
+
+    :return:
+    """
+    print("Hello")
+
+
+greet_person()
+
+
+def greet_person(name):
+    """
+    This function is used to greet people
+    another line
+    it's a good habit
+    with addded concat name
+    :return:
+    """
+    print("Hello" + ' ' + name)
+
+
+greet_person('Boob')
