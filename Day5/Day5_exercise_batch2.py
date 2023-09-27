1cf13e5bd6436bf7bf907d8868001dc43453f681
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-27 23:24:04 +0200

    Add files via upload

--- Day5/Day5_exercise_batch2.py
+++ Day5/Day5_exercise_batch2.py
@@ -6,3 +6,26 @@ def greet():
 
 # 2
 
+def welcome(name):
+    print(f"Welcome {name}!")
+
+
+welcome('Luke')
+
+# correct:
+
+def welcome(name):
+    print(f"Welcome {name}!")
+
+name = ('Luke')
+
+
+
+# 3
+
+
+def square(number):
+    print(number ** 2)
+
+
+number = 9
