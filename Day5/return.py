3934cc1780968fd6c1e07fa18b5f6a8d6e04a7cc
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-09-28 21:51:32 +0200

    Add files via upload

---
+++ Day5/return.py
@@ -0,0 +1,26 @@
+def multiply(number1, number2):
+    return number1 * number2
+
+
+multiply(2, 4)
+
+print(multiply(2, 4))
+
+
+def multiply(number1, number2):
+    total = number1 * number2
+    return total
+
+result = multiply(5, 200)
+print(result)
+
+
+
+def multiply(number1, number2):
+    total = number1 * number2
+    print(total)
+    return total
+
+result = multiply('5', '200')
+print(result)
+
