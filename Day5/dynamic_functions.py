001deb08f7368e1a064ef5239ad884d143af7e9e
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-10-02 23:55:18 +0200

    Add files via upload

--- Day5/dynamic_functions.py
+++ Day5/dynamic_functions.py
@@ -1,6 +1,35 @@
 def check_3_digits(number):
     return number in range(100, 1000)
 
+sum = 526 + 311
+result = check_3_digits(sum)
+print(result)
 
-result = check_3_digits(111)
+
+
+def check_3_digits(list1):
+    for number in list:
+        if number in range(100, 1000):
+            return True
+        else:
+            pass
+
+
+
+sum = 526 + 311
+result = check_3_digits(sum)
+print(result)
+
+
+def check_3_digits(list1):
+    for number in list:
+        if number in range(100, 1000):
+            return  #  return true kills a function
+        else:
+            pass
+
+
+
+sum = 526 + 311
+result = check_3_digits(sum)
 print(result)
