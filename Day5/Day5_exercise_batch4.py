bc8060ffe079a4568ce2f4a20fceb97c1c0ed86a
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-10-04 02:50:55 +0200

    Add files via upload

---
+++ Day5/Day5_exercise_batch4.py
@@ -0,0 +1,15 @@
+# 1
+
+def all_positives(list1):
+    for n in list1:
+        if n < 0:
+            return False
+        else:
+            pass
+    return True
+
+
+numbers = [-1, 0, 2, 3]
+
+# 2
+
