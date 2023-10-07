6cc1f47951806d8896b2e54db6ca831c34515f3e
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-10-07 23:41:49 +0200

    Add files via upload

--- Day5/Day5_exercise_batch4.py
+++ Day5/Day5_exercise_batch4.py
@@ -1,5 +1,5 @@
 # 1
-
+'''
 def all_positives(list1):
     for n in list1:
         if n < 0:
@@ -10,9 +10,9 @@ def all_positives(list1):
 
 
 numbers = [-1, 0, 2, 3]
-
+'''
 # 2
-
+'''
 def sum_less(list1):
     sum_n = 0.
     for n in list1:
@@ -24,7 +24,21 @@ def sum_less(list1):
 
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1000]
-
+'''
 
 # 3
+
+def count_even(list2):
+    number = 0
+    for n in list2:
+        if n % 2 == 0:
+            number += 1
+        else:
+            continue
+    return number
+
+numbers = [1 ,2, 3, 4, 5, 6, 7 ,8 ,9 ,10,11,12,13,14,15,16,17,18,19,20]
+
+print(count_even(numbers))
+
 
