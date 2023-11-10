b6eef3b7cf13ea23223a3f34625c84cb7e0d4bd8
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-11-10 23:56:40 +0100

    Add files via upload

--- Day5/dynamic_functions2.py
+++ Day5/dynamic_functions2.py
@@ -4,3 +4,11 @@ def check_4_digits(number):
 sum = 999 + 12
 result = check_4_digits(sum)
 print(result)
+
+
+def check_3_digits(number):
+    return number in range (100,1000)
+sum1= 12+445
+result = check_3_digits(sum1)
+print(result)
+
